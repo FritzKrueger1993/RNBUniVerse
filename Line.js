c5b2503b234a3e2d71ddb16fc95845bd2aa51dcd
FritzKrueger1993 <109686983+FritzKrueger1993@users.noreply.github.com>
2023-07-21 15:49:49 +0200

    Add files via upload

---
+++ Line.js
@@ -0,0 +1,22 @@
+class Line{
+   constructor() {
+     this.x = random(width);
+     this.y = random(height);
+     this.color = random(360);
+     this.degrees = 0;
+     this.growthX = 1;
+     this.growthY = 1;
+     this.degreesRate = 1;
+     this.degressJump = 5;
+   }
+   move(){
+     this.x = this.x + this.growthX;
+     this.y = this.y + this.growthY;
+     
+   }
+   show(){
+     stroke(this.color);
+     ellipse(this.x,this.y,1,1);
+   }
+  
+}
---
+++ linegrowth.js
@@ -0,0 +1,16 @@
+let line1
+
+function setup() {
+  createCanvas(500,500);
+  background(0);
+  line1 = new Line();
+}
+
+
+function draw() {
+  
+  line1.show();
+  line1.move();
+
+  
+}
