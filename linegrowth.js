55a8649750e3f1f42edb9350cfa77cf70d47f982
FritzKrueger1993 <109686983+FritzKrueger1993@users.noreply.github.com>
2023-07-21 15:53:28 +0200

    Update linegrowth.js

--- linegrowth.js
+++ linegrowth.js
@@ -2,15 +2,21 @@ let line1
 
 function setup() {
   createCanvas(500,500);
+  colorMode(HSB, 360);
   background(0);
   line1 = new Line();
 }
 
 
 function draw() {
-  
+ 
   line1.show();
   line1.move();
 
   
 }
+
+function mousePressed() {
+  line1 = new Line();
+}
+
